385e10bf061133f3820712854e2e83d43197f3c0
DeepMakhasana <deepmakhasana1013@gmail.com>
2024-12-19 12:18:06 +0530

    update deployment changes

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 // Import required modules
-const serverless = require("serverless-http");
+import ServerlessHttp from "serverless-http";
 import app from "./src/app";
-import config from "./src/config";
+// import config from "./src/config";
 
 // Start server
-if (config.port === "development") {
-    const PORT = config.port || 8000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-} else {
-    module.exports.handler = serverless(app);
-}
+// if (config.port === "development") {
+//     const PORT = config.port || 8000;
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     });
+// }
+export const handler = ServerlessHttp(app);
